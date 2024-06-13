31d55d7f9960de66ff0356e4b581384aac139ccb
Rick Henry <rjh@rickhenry.uk>
2024-06-13 16:58:12 +0100

    feat: start adding tags with new versions

--- scripts/update_dockerfile.sh
+++ scripts/update_dockerfile.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo "Fetching latest Bookstack release from GitHub API"
+
 BOOKSTACK_VERSION=$(curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
@@ -7,9 +9,16 @@ BOOKSTACK_VERSION=$(curl -L \
   jq -r .tag_name
 )
 
-echo "Latest: ${BOOKSTACK_VERSION}. Updating.."
+echo "Found latest version: ${BOOKSTACK_VERSION}"
 
+# Get the root of the Git repository in order to correctly path e.g. Dockerfile
+GIT_ROOT=$(git rev-parse --show-toplevel)
+
+echo "Updating Dockerfile.."
 sed \
   -i '' \
   -e "s/^ENV BOOKSTACK_VERSION=.*/ENV BOOKSTACK_VERSION=${BOOKSTACK_VERSION}/" \
-  Dockerfile
+  "${GIT_ROOT}/Dockerfile"
+
+git add "${GIT_ROOT}/Dockerfile"
+git commit -S -m "feat: update Dockerfile to use Bookstack ${BOOKSTACK_VERSION}"
